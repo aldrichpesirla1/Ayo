ce79541e66b3beec165e07f33faaf02ced7c5c2e
Christianmarkgonzales <80030271+Christianmarkgonzales@users.noreply.github.com>
2021-04-15 15:08:44 +0800

    Add files via upload
    //edited text design on modals

--- RN/Ayo/src/screens/productListScreen.js
+++ RN/Ayo/src/screens/productListScreen.js
@@ -166,7 +166,7 @@ const productList = () => {
               <TouchableOpacity style={{margin:15 , alignSelf:'flex-end', position: 'absolute'}} onPress = {() => setSuccessVisible(!successVisible)}>
                       <Fontisto name="close" size={30}/>
               </TouchableOpacity>
-              <Text style={{alignSelf:'center', fontSize:28, fontWeight:'bold'}}> Success! </Text>
+              <Text style={{alignSelf:'center', fontSize:28, color:"#00B300", letterSpacing: 0.5, fontWeight:'bold'}}> SUCCESS! </Text>
               </View>
               <View style={{alignItems:'center'}}>
                 <Image source={require('../assets/success.jpg')}
@@ -193,14 +193,14 @@ const productList = () => {
               <TouchableOpacity style={{margin:15 , alignSelf:'flex-end', position: 'absolute'}} onPress = {() => setFailVisible(!failVisible)}>
                       <Fontisto name="close" size={30}/>
               </TouchableOpacity>
-              <Text style={{alignSelf:'center', fontSize:25, fontWeight:'bold'}}> Fail! </Text>
+              <Text style={{alignSelf:'center', fontSize:25, color:"#E60000", letterSpacing: 3, fontWeight:'bold'}}> FAIL! </Text>
               </View>
               <View style={{alignItems:'center'}}>
                 <Image source={require('../assets/warning.png')}
                 style={{height:100, width:90, marginVertical:2}}>
                 </Image>
               </View>
-              <Text style={{marginVertical:1, fontSize:20, textAlign:'center'}}>
+              <Text style={{marginVertical:1, fontSize:20,  textAlign:'center'}}>
                 Product has not been added
               </Text> 
               
