45e0c9c39cfe36a5cbf4d94105b5e86543cbd3cf
Christianmarkgonzales <80030271+Christianmarkgonzales@users.noreply.github.com>
2021-04-20 14:33:21 +0800

    Add files via upload

---
+++ RN/Ayo/src/modals/addProductFail.js
@@ -0,0 +1,39 @@
+import React, {useLayoutEffect, useState} from 'react'
+import { View, Text, Image, StyleSheet, TouchableOpacity, Modal, Alert, Button} from 'react-native'
+
+export default function AddProductFail() {
+return(
+
+        <View>
+              <View style={{alignItems:'flex-start'}}>
+                <Image source={require('../assets/warning.png')}
+                style={styles.image}>
+                </Image>
+                <Text style={styles.text}>
+                Product has not been added
+              </Text>
+              </View>
+
+        </View>
+)
+}
+const styles=StyleSheet.create({
+    text:{
+        marginVertical:1,
+        marginHorizontal: 5,
+        fontSize:20, 
+        textAlign:'center'
+
+    },
+    image:{
+        height:40, 
+        width:45, 
+        marginVertical:5, 
+        position: 'relative', 
+        alignSelf:'center'
+    }
+
+})
+
+
+
